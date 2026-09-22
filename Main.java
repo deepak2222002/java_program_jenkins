4ddf8384109bda2351fe163503b5eab530ff79da
deepak2222002 <129276958+deepak2222002@users.noreply.github.com>
2026-09-22 16:38:48 +0530

    add more lines

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 import java.util.*;
-public class Main
-{
+public class Main {
 	public static void main(String[] args) {
-		PriorityQueue qp=new PriorityQueue();
-		
+		PriorityQueue qp = new PriorityQueue();
 		qp.offer("B");
 		qp.offer("A");
 		qp.offer("C");
@@ -13,14 +11,16 @@ public class Main
 		qp.offer("G");
 		qp.offer("H");
 		qp.offer("I");
-		
+		qp.offer("J");
+		qp.offer("K");
 		System.out.println(qp.poll());
 		System.out.println(qp.poll());
 		System.out.println(qp.poll());
 		System.out.println(qp.poll());
 		System.out.println(qp.poll());
 		System.out.println(qp.poll());
-
-		
+		System.out.println(qp.poll());
+		System.out.println(qp.poll());
+		System.out.println(qp.poll());
 	}
 }
